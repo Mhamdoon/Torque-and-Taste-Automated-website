2535286cc0bee1070861e2591a2f90c678465e17
unknown <howtohowto44@gmail.com>
2026-06-25 02:31:37 +0500

    Register models in admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ServiceWindow
+from .models import Customer
+from .models import Order
+from .models import OrderItem
+from .models import MenuItem
+
+admin.site.register(ServiceWindow)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(MenuItem)
 
 # Register your models here.
